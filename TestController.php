9fafded8bb194f1eaf94fd0622ed3c03783358af
Abhijith <abhijith.k@applab.qa>
2025-03-26 11:18:49 +0530

    TS-3: Updates in testcontroller

--- TestController.php
+++ TestController.php
@@ -1,8 +1,30 @@
 <?php
 
-echo "Hello World";
+// Display a simple message
+echo "Hello World<br>";
 
-$_COOKIE['test'] = 'test';
-echo $_COOKIE['test'];
+// Set a cookie
+setcookie('test', 'test', time() + 3600, '/'); // Cookie expires in 1 hour
+
+// Check if the cookie is set and display its value
+if (isset($_COOKIE['test'])) {
+    echo "Cookie value: " . $_COOKIE['test'] . "<br>";
+} else {
+    echo "Cookie is not set.<br>";
+}
+
+// Add a function to display the current date and time
+function displayDateTime() {
+    return date('Y-m-d H:i:s');
+}
+
+// Display the current date and time
+echo "Current Date and Time: " . displayDateTime() . "<br>";
+
+// Add a simple loop to display numbers from 1 to 3
+echo "Numbers from 1 to 3: ";
+for ($i = 1; $i <= 3; $i++) {
+    echo $i . " ";
+}
 
 ?>
